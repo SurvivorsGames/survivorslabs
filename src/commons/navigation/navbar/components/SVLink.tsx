20f87b7520d6410c7c09e8e81d43452e98a90d27
jacepeda <jacepeda@indracompany.com>
2022-07-08 15:27:37 -0500

    navlink

---
+++ src/commons/navigation/navbar/components/SVLink.tsx
@@ -0,0 +1,24 @@
+import { Button, makeStyles, withStyles } from "@mui/material";
+import { PRIMARY, SECONDARY, TEXT, THIRD } from "commons/theme/colors";
+
+interface ISVLink {
+	children: any;
+	onClick: any;
+}
+
+export default function SVLink(props: ISVLink) {
+	const { children, onClick } = props;
+	return (
+		<Button
+			onClick={onClick}
+			style={{
+				color: PRIMARY,
+				textDecoration: "underline",
+				display: "inline-block",
+				padding: 20,
+			}}
+		>
+			{children}
+		</Button>
+	);
+}
--- src/modules/dashboard/DashBoard.tsx
+++ src/modules/dashboard/DashBoard.tsx
@@ -1,12 +1,47 @@
 import SVBanner from "commons/components/branch/SVBanner";
-import SVNavbar from "commons/navigation/navbar/SVNavbar";
+import SVLink from "commons/navigation/navbar/components/SVLink";
+import { TEXT } from "commons/theme/colors";
 import "../../App.css";
 
 export default function DashBoard() {
+	const itemsNav = [
+		{
+			label: "Nosotros",
+			action: () => {},
+		},
+		{
+			label: "Aplicaciones",
+			action: () => {},
+		},
+		{
+			label: "Juegos",
+			action: () => {},
+		},
+	];
+
 	return (
-		<div className="App">
+		<>
 			<SVBanner />
-			{/* <SVNavbar></SVNavbar> */}
-		</div>
+			<div
+				style={{
+					position: "absolute",
+					bottom: 30,
+					paddingLeft: "600px",
+				}}
+			>
+				<label
+					style={{
+						color: TEXT,
+						textAlign: "center",
+						alignSelf: "center",
+					}}
+				>
+					...Sitio en construcción...
+				</label>
+				{/* {itemsNav.map((item) => (
+							<SVLink onClick={item.action}>{item.label}</SVLink>
+						))} */}
+			</div>
+		</>
 	);
 }
