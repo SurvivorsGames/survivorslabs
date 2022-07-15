d1e3a1733a1b0dda386db5dec6e777b5a8c1d5ab
jacepeda <jacepeda@indracompany.com>
2022-07-14 20:55:59 -0500

    Newest

--- src/modules/dashboard/components/Newest.tsx
+++ src/modules/dashboard/components/Newest.tsx
@@ -1,5 +1,6 @@
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import { NewestBanner } from "assets";
+import { TEXT, SECONDARY } from "commons/theme/colors";
 
 export default function Newest() {
 	return (
@@ -16,11 +17,55 @@ export default function Newest() {
 						style={{ alignSelf: "center" }}
 						src={NewestBanner}
 					/>
-					<div style={{ position: "absolute", right: 200 }}>
-						<h1 style={{ textAlign: "center" }}>
+					<Grid
+						item
+						style={{
+							position: "absolute",
+							right: 20,
+							width: "50%",
+						}}
+						xs={6}
+						md={6}
+						lg={6}
+					>
+						<h1
+							style={{
+								textAlign: "center",
+								fontFamily: "Graduate",
+							}}
+						>
 							Nueva aplicación
 						</h1>
-					</div>
+						<p
+							style={{
+								textAlign: "center",
+								fontFamily: "Roboto Mono",
+							}}
+						>
+							¿Te gustaría saber si es seguro subirte a un
+							vehículo que ha aceptado el servicio?
+							<br />
+							<br /> En nuestra aplicación puedes buscar los
+							reportes que ha tenido un usuario por las placas de
+							su vehículo y es tu decisión tomar o no el servicio.
+							Asi mismo, puedes reportar cualquier caso, cuando te
+							hayas sentido incomoda o hayas detectado actividad
+							sospechosa durante tu viaje.
+						</p>
+
+						<Button
+							style={{
+								fontFamily: "Graduate",
+								textAlign: "center",
+								display: "block",
+								backgroundColor: SECONDARY,
+								color: TEXT,
+							}}
+							href="#"
+						>
+							DESCARGA AHORA
+						</Button>
+					</Grid>
 				</div>
 			</Grid>
 		</Grid>
